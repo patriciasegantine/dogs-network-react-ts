92439a067afa39bc393548d61e4af1b8c2193eba
Patricia Segantine <pnsegantine@gmail.com>
2022-12-15 17:07:21 +0000

    setup project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './global.module.css'
+
+function App() {
+
+  return (
+    <div className={styles.container}>
+      Hello Word!
+    </div>
+  )
+}
+
+export default App
+ 
