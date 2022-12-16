f1d2d33dcdb5f8c460343b31769abadad932968a
Patricia Segantine <pnsegantine@gmail.com>
2022-12-16 20:29:22 +0000

    configuring fetch API

--- src/components/login/login-form.jsx
+++ src/components/login/login-form.jsx
@@ -1,37 +1,52 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Input } from '../form/input'
 import { Button } from '../form/button'
 import { UseForm } from '../../hooks/useForm.js'
-
-
+import { TOKEN_POST, USER_GET } from '../../api.js'
 
 export const LoginForm = () => {
 
-  const username = UseForm('email')
-  const password = UseForm('password')
+  const username = UseForm()
+  const password = UseForm()
 
-  console.log('username', username.validate)
-  console.log('password', password.validate)
+  const getUser = async (token) => {
+    const {url, options} = USER_GET(token)
 
-  const handleSubmit = (event) => {
+    const response = await fetch(url, options)
+    const json = await response.json()
+
+    console.log(json)
+  }
+
+
+  const handleSubmit = async (event) => {
     event.preventDefault()
 
     if(username.validate() && password.validate()) {
 
-      const url = 'https://dogsapi.origamid.dev/json/jwt-auth/v1/token'
-      fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify()
-      }).then(response => {
-        console.log(response)
-        return response.json
-      }).then(json => console.log(json))
+    const {url, options } = TOKEN_POST({
+      username: username.value,
+      password: password.value
+    })
+
+      const response = await  fetch(url, options)
+      const json  = await response.json()
+
+      window.localStorage.setItem('token', json.token )
+      await getUser(json.token)
     }
   }
+
+  useEffect(() => {
+    const token = window.localStorage.getItem('token')
+
+    console.log('token Effect', token)
+
+    if(token) {
+      getUser(token)
+    }
+  }, [])
 
   return (
     <section className={'container'}>
@@ -42,8 +57,6 @@ export const LoginForm = () => {
           type={'text'}
           name={'username'}
           {...username}
-          // value={username}
-          // onChange={(event) => setUsername(event.target.value)}
         />
 
         <Input
@@ -51,8 +64,6 @@ export const LoginForm = () => {
           type={'password'}
           name={'password'}
           {...password}
-          // value={password}
-          // onChange={(event) => setPassword(event.target.value)}
         />
 
         <Button>Send</Button>
